018f20f31ac1b9c94fec21f3c11191897494bf92
wilderbeest <wilderbeest@davidjragsdale.com>
2020-11-12 12:22:02 -0500

    Fix lint

--- examples/cli/index.js
+++ examples/cli/index.js
@@ -5,5 +5,9 @@ const cs = countSlowly({ stepDuration: 100 });
 const startWith = 1;
 cs.set(startWith);
 process.stdout.write(startWith.toString(), 'utf-8');
-cs.onUpdate(temp => process.stdout.write(temp.toString(), 'utf-8'));
-cs.update(9);
+cs.onUpdate((temp) => process.stdout.write(` ${temp}`, 'utf-8'));
+
+setTimeout(() => {
+  cs.hurry();
+}, 2000);
+cs.update(100);
